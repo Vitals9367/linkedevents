93b70681c0a7e4b3fc79cacd2b3d0fe11b2e0d08
Juha Louhiranta <juha.louhiranta@anders.fi>
2023-05-03 16:36:45 +0300

    Fix typo
    Refs LINK-1274

--- events/api.py
+++ events/api.py
@@ -3463,7 +3463,7 @@ class EventViewSet(
         # For bulk update, the editable queryset is filtered in filter_queryset
         # method
 
-        # Prevent changing existing events to a state that user doe snot have write permissions
+        # Prevent changing existing events to a state that user does not have write permissions
         for event_data in serializer.validated_data:
             _, org = self.user_data_source_and_organization
             if "publisher" in event_data:
