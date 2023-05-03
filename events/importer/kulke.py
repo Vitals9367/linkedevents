71800d6586383f5a43830f173d143cb0501d3901
Juha Louhiranta <juha.louhiranta@anders.fi>
2023-05-03 14:29:34 +0300

    Changed in kulke.py
    Refs LINK-1274

--- events/importer/kulke.py
+++ events/importer/kulke.py
@@ -441,6 +441,8 @@ class KulkeImporter(Importer):
         elif Keyword.objects.get(id="yso:p26626") in event["keywords"]:
             # "Etäosallistuminen" is also mapped to our new fancy "Tapahtuma vain internetissä." location
             event["location"]["id"] = INTERNET_LOCATION_ID
+        elif "virtuaalinen" in loc_name.lower():
+            event["location"]["id"] = INTERNET_LOCATION_ID
         else:
             logger.warning(
                 "No match found for place '%s' (event %s)"
@@ -483,15 +485,18 @@ class KulkeImporter(Importer):
 
         eid = int(event_el.attrib["id"])
         self.eventcodes_set.add(text_content("servicecode"))
+
+        event = events[eid]
+
         if text_content("servicecode") != "Pelkkä ilmoitus" and not is_course:
+            event["kulke_new"] = True
             # Skip courses when importing events
-            return False
+            # return False
 
         if self.options["single"]:
             if str(eid) != self.options["single"]:
                 return False
 
-        event = events[eid]
         if is_course:
             event["type_id"] = 2
         else:
